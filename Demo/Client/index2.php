068901b8fce731e4b42f75617b34aa4a9dbce03b
kaluo <284474102@qq.com>
2018-07-10 11:22:37 +0800

    优化

--- Demo/Client/index2.php
+++ Demo/Client/index2.php
@@ -3,6 +3,36 @@ header('Content-Type:text/html; charset=utf-8');
 
 $sso_url = 'https://user.aiku.fun/index.php'; //你SSO所在的域名,不是当前项目地址
 $url = 'https://www.aiku.fun/index2.php';
+
+#测试网址:     http://localhost/blog/testurl.php?id=5
+
+//获取域名或主机地址
+echo $_SERVER['HTTP_HOST']."<br>"; #localhost
+
+//获取网页地址
+echo $_SERVER['PHP_SELF']."<br>"; #/blog/testurl.php
+
+//获取网址参数
+echo $_SERVER["QUERY_STRING"]."<br>"; #id=5
+
+//获取用户代理
+echo $_SERVER['HTTP_REFERER']."<br>";
+
+//获取完整的url
+echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
+#http://localhost/blog/testurl.php?id=5
+
+//包含端口号的完整url
+echo 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+#http://localhost:80/blog/testurl.php?id=5
+
+//只取路径
+$url='http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"];
+echo dirname($url);
+#http://localhost/blog
+
+
 if(isset($_COOKIE['sign'])){
     exit("欢迎您{$_COOKIE['sign']} <a href='{$sso_url}?logout=1&callback={$_SERVER['HTTP_REFERER']}'>退出</a>");
 }else{
