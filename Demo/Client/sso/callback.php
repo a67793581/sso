f6337f7cde4ea6296cb36fafae7d71c813111738
kaluo <284474102@qq.com>
2018-07-11 19:51:56 +0800

    测试退出校验

--- Demo/Client/sso/callback.php
+++ Demo/Client/sso/callback.php
@@ -16,7 +16,7 @@ if(empty($_GET)){
             break;
         case 'logout':
             var_dump($_GET);
-            if(!empty($_GET['time']) && !empty($_GET['sign'])){
+            if(isset($_GET['time']) && isset($_GET['sign']) ){
                 $params = array('time'=>$_GET['time'],'type'=>'logout');
                 $res = $core->logout($_GET['sign'],$params);
                 if($res){
