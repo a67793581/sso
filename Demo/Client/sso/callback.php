cc8f857f51bbbc77ce4813c728aac045abdee664
kaluo <284474102@qq.com>
2018-07-18 20:44:18 +0800

    使用ajax完成登录和退出通知

--- Demo/Client/sso/callback.php
+++ Demo/Client/sso/callback.php
@@ -6,21 +6,24 @@ if(empty($_GET)){
 }else{
     require_once 'Client_core.php';
     $core = new Client_core();
+    $callback = empty($_GET['callback']) ? 'function' : $_GET['callback'];
+    $callback2 = empty($_GET['callback']) ? '{}' : '';
     switch ($_GET['type']){
         case 'login':
+
             if(!empty($_GET['time']) && !empty($_GET['sign']) && !empty($_GET['code'])){
                 $params = array('time'=>$_GET['time'],'type'=>'login','code'=>$_GET['code']);
                 $user = $core->login($_GET['code'],$params,$_GET['sign']);
                 if(is_int($user)){
-                    exit($_GET['callback'] ." ($user)");
+                    exit($callback ." ($user)" . $callback2);
                 }else{
                     foreach($user as $key=>$val){
                         setcookie($key,$val,0,'/');
                     }
-                    exit($_GET['callback'] .' ("登录成功")');
+                    exit($callback .' ("登录成功")' . $callback2);
                 }
             }else{
-                exit($_GET['callback'] .' ("非法请求")');
+                exit($callback .' ("非法请求")' . $callback2);
             }
             break;
         case 'logout':
@@ -29,12 +32,12 @@ if(empty($_GET)){
                 $res = $core->logout($_GET['sign'],$params);
                 if($res){
                     setcookie('sign','',0,'/');
-                    exit($_GET['callback'] .' ("退出成功")');
+                    exit($callback .' ("退出成功")' . $callback2);
                 }else{
-                    echo $_GET['callback'] .' ("校验失败")';
+                    echo $callback .' ("校验失败")' . $callback2;
                 }
             }else{
-                echo $_GET['callback'] .' ("非法请求")';
+                echo $callback .' ("非法请求")' . $callback2;
             }
             break;
     }
